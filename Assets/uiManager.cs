c81bbb1ea7b180235febc72b0bd8ee042f92c005
andyshep11 <30866297+andyshep11@users.noreply.github.com>
2017-08-09 21:02:28 +0100

    Main and settings menu added

---
+++ Assets/uiManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiManager : MonoBehaviour {
+
+  public Canvas mainMenu;
+  public Canvas settingsMenu;
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void showSettingsMenu () {
+    mainMenu.enabled = false;
+    settingsMenu.enabled = true;
+  }
+
+  public void hideSettingsMenu()
+  {
+    settingsMenu.enabled = false;
+    mainMenu.enabled = true;
+  }
+
+}
